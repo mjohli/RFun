8c6b06e817d00542ce6747a3ef74d73182710136
mjohli <mohlinge@mail.uni-mannheim.de>
2022-10-27 10:47:44 +0200

    Updated ggplot2 themes

--- data-raw/moTheme.R
+++ data-raw/moTheme.R
@@ -1,8 +1,7 @@
 #' My Theme
 #'
 #' A very clean theme for ggplot2 with no axes and no background
-moTheme <- ggplot2::theme(text = ggplot2::element_text(size = 20),
-                 panel.grid = ggplot2::element_blank(),
+moTheme <- ggplot2::theme(panel.grid = ggplot2::element_blank(),
                  panel.border = ggplot2::element_blank(),
                  panel.background = ggplot2::element_rect(fill = "white",
                                                  colour = "white",
