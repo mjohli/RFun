19b94eb8b4f89995984a6918108c0674a31cfa79
mjohli <michael.ohlinger@unisg.ch>
2024-06-12 15:58:36 +0200

    Arguments now should be passed onto install_github()

--- R/updateRFun.R
+++ R/updateRFun.R
@@ -1,11 +1,12 @@
 #' Convenience function for updating RFun
 #'
 #' Unloads RFun, installs the newest version and attaches it again
+#' @param ... Arguments to be passed on to `install_github()`
 #' @export
 #'
 
-updateRFun <- function(){
+updateRFun <- function(...){
     unloadNamespace("RFun")
-    devtools::install_github("mjohli/RFun")
+    devtools::install_github("mjohli/RFun", ...)
     library(RFun)
 }
