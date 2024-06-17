1cbbdb95904b305d3112002baa511d324915453a
mjohli <michael.ohlinger@unisg.ch>
2024-06-17 14:30:04 +0200

    Incomplete function, maybe of use for later

---
+++ R/getDateTz.R
@@ -0,0 +1,12 @@
+#' !!Preliminary!! function to extract times from the GPIPP!!
+#'
+#' First steps to extract times and time zones from older answers to the GPIPP since it has different date formats
+#' @param x The date string
+#' @export
+#'
+
+getDateTz <- function(x){
+    tzs <- substr(x, 21, 23)
+    x <- paste0(substr(x, 5, 20), substr(x, 25, 29))
+    parse_date_time(x, orders = "bdHMSY", tz = tzs)
+}
