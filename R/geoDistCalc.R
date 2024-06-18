fad160cc08aa925ba21381c220380863a689216d
mjohli <michael.ohlinger@unisg.ch>
2024-06-18 11:31:42 +0200

    Added function to calculate distances

---
+++ R/geoDistCalc.R
@@ -0,0 +1,23 @@
+#' Calculates distances between all given shapes
+#'
+#' Calculates direct distances between every pairing of the given shapes. If necessary, simulates random variables for each geographic entity and gives them in a data frame.
+#' @param sf Shapefile to calculate distances on
+#' @param vars Name(s) of the variables that should be simulated
+#' @returns Gives a matrix containing all distances between all pairings. If variables are simulated, gives a list containing the distances and a data frame with the variables together with the GEOID.
+#' @export
+#'
+
+geoDistCalc <- function(sf, vars = NULL){
+    cbsaShp <- read_sf(sf)
+    cbsaShp <- subset(cbsaShp, select = "GEOID")
+    cbsaShp <- st_centroid(cbsaShp)
+    cbsaDistances <- as.data.frame(st_distance(cbsaShp))
+    colnames(cbsaDistances) <- rownames(cbsaDistances) <- cbsaShp$GEOID
+    if(!is.null(vars)){
+        cbsaShp <- st_drop_geometry(cbsaShp)
+        cbsaShp[vars] <- matrix(rnorm(length(cbsaShp$GEOID) * length(vars)), ncol = length(vars))
+        return(list(df = cbsaShp, dists = cbsaDistances))
+    } else{
+        return(cbsaDistances)
+    }
+}
