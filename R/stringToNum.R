f63483da8afd78231ea3c18ad4e392ac75bbf921
mjohli <michael.ohlinger@unisg.ch>
2024-06-21 13:19:11 +0200

    Made error checking optional to increase performance

--- R/stringToNum.R
+++ R/stringToNum.R
@@ -11,15 +11,20 @@
 #' stringToNum(c("abc", "abc123", "abc12de", "twelve"))
 #'
 
-stringToNum <- function(x, n = c(1, 2), words = TRUE, strOut = FALSE, encodeCleaner = TRUE){
+stringToNum <- function(x, n = c(1, 2), words = TRUE, strOut = FALSE, encodeCleaner = TRUE, wtnErrorCatch = FALSE){
     if(encodeCleaner){
         x <- stringi::stri_trans_general(str = x, id = "latin-ascii")
     }
     if(words){
-        for(i in 1:length(x)){
-            tryCatch(x[i] <- wordstonumbers::words_to_numbers(x[i]), error = function(e) cat(paste0(e, "i = ", i, "\nx[i] = ", x[i])))
+        if(wtnErrorCatch){
+            for(i in 1:length(x)){
+                tryCatch(x[i] <- wordstonumbers::words_to_numbers(x[i]), error = function(e) cat(paste0(e, "i = ", i, "\nx[i] = ", x[i])))
+            }
+            x <- ifelse(is.null(x), NA, x)
+        } else{
+            x <- Vectorize(FUN = wordstonumbers::words_to_numbers)(x)
         }
-        x <- ifelse(is.null(x), NA, x)
+
     }
     x <- gsub(pattern = paste0("^\\D*(\\d{", paste(n, collapse = ","), "})(\\D.*)?$"), replacement = "\\1", x)
     if(!strOut){
