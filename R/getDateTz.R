6f64704cdb1d5900e9c5a9f318e932dd0d14c114
mjohli <michael.ohlinger@unisg.ch>
2024-06-24 12:23:20 +0200

    Date conversion function now running

--- R/getDateTz.R
+++ R/getDateTz.R
@@ -1,12 +1,27 @@
-#' !!Preliminary!! function to extract times from the GPIPP!!
+#' Function to extract times from the GPIPP
 #'
-#' First steps to extract times and time zones from older answers to the GPIPP since it has different date formats
-#' @param x The date string
+#' Converts to date and converts the more difficult strings in a second step if necessary
+#' @param x Vector full of date strings
 #' @export
 #'
 
 getDateTz <- function(x){
-    tzs <- substr(x, 21, 23)
-    x <- paste0(substr(x, 5, 20), substr(x, 25, 29))
-    lubridate::parse_date_time(x, orders = "bdHMSY", tz = tzs)#Lubridate not yet in DESCRIPTION
+    res <- as.POSIXct(rep(NA, length(x)))
+    for(i in 1:length(x)){
+        v <- try(as.POSIXct(x[i]), silent = TRUE)
+        if("try-error" %in% class(v)){
+            tz <- gsub("^[A-Z][a-z]{2}\\s[A-Z][a-z]{2}\\s{1,2}\\d{1,2}\\s\\d{2}:\\d{2}:\\d{2}\\s([A-Z]{3})\\s\\d{3,4}$", "\\1", x[i])
+            m <- match(gsub("^[A-Z][a-z]{2}\\s([A-Z][a-z]{2})\\s{1,2}\\d{1,2}\\s\\d{2}:\\d{2}:\\d{2}\\s[A-Z]{3}\\s\\d{3,4}$", "\\1", x[i]), month.abb)
+            tidyd <- paste0(gsub("^[A-Z][a-z]{2}\\s[A-Z][a-z]{2}\\s{1,2}\\d{1,2}\\s\\d{2}:\\d{2}:\\d{2}\\s[A-Z]{3}\\s(\\d{3,4})$", "\\1-", x[i]), m,
+                            gsub("^[A-Z][a-z]{2}\\s[A-Z][a-z]{2}\\s{1,2}(\\d{1,2})\\s(\\d{2}:\\d{2}:\\d{2})\\s[A-Z]{3}\\s\\d{3,4}$", "-\\1 \\2", x[i]))
+            tidyd <- gsub("-(\\d)-", "-0\\1-", tidyd)
+            v <- try(as.POSIXct(tidyd, tz = tz), silent = TRUE)
+            if("try-error" %in% class(v)){
+                v <- NA
+            }
+        }
+        res[i] <- as.POSIXct(v)
+    }
+    return(res)
 }
+
