2d2ed24abb6d263aae1c4adc0526be47ad1adcbb
mjohli <michael.ohlinger@unisg.ch>
2024-09-11 13:31:53 +0200

    Added dataframe simulation function

---
+++ R/simDat.R
@@ -0,0 +1,16 @@
+#' Function to simulate simple data
+#'
+#' Generates a dataframe with one row for each id, normally distributed
+#' variables for 'vars', and uniform dichotomous variables for 'dichVars'
+#' @param ids Vector of ids to identify each row
+#' @param vars Vector of variable names for continuous normally distributed variables
+#' @param dichVars Vector of variable names for dichotomous uniformly distributed variables
+#' @export
+#'
+simDat <- function(ids, vars, dichVars = NULL){
+    dat <- data.frame(id = as.factor(ids))
+    n <- length(ids)
+    dat[vars] <- matrix(rnorm(n * length(vars)), ncol = length(vars))
+    dat[dichVars] <- matrix(sample(c(TRUE, FALSE), n * length(dichVars), replace = TRUE), ncol = length(dichVars))
+    return(as.data.frame(dat))
+}
